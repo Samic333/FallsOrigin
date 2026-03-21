781f696720390c1042c376e24d014a02c6b0f787
Samuel Woldegiorgies <samuel.woldegiorgies@icloud.com>
2026-03-21 18:02:42 +0300

    Fix PDO constant typos (ATTR_ERRMODE, ERRMODE_EXCEPTION) in db.php

--- includes/db.php
+++ includes/db.php
@@ -8,7 +8,7 @@ class DB {
     private function __construct() {
         $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
         $options = [
-            PDO::ATTR_ERR_MODE            => PDO::ERR_MODE_EXCEPTION,
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
